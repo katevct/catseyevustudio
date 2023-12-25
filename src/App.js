90e610083df8e63d530cc445da6ec17b26939dff
katevct <kate.vct@outlook.com>
2023-12-25 13:06:05 -0800

    unified

---
+++ src/App.js
@@ -0,0 +1,282 @@
+import { Link as ScrollLink, Element } from 'react-scroll';
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import Mooo from './Mooo';
+import Yogi from './Yogi';
+import Studio from './Studio';
+import './App.css';
+import ContactFooter from './ContactFooter';
+
+function getRandomPastelColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function App() {
+  const [activeComponent, setActiveComponent] = useState(null);
+  const [colorToggle, setColorToggle] = useState(false);
+  
+  const randomPastelColorMooo = getRandomPastelColor();
+  const randomPastelColorYogi = getRandomPastelColor();
+  const randomPastelColorStudio = getRandomPastelColor();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Get the header element
+      const header = document.getElementById('app-header');
+
+      // Add or remove the 'fixed-header' class based on the scroll position
+      if (window.pageYOffset > 80) {
+        header.classList.add('fixed-header');
+      } else {
+        header.classList.remove('fixed-header');
+      }
+    };
+
+     // Attach the scroll event listener
+     window.addEventListener('scroll', handleScroll);
+
+     // Clean up the event listener on component unmount
+     return () => {
+       window.removeEventListener('scroll', handleScroll);
+     };
+   }, []);
+
+   const toggleComponent = (component) => {
+    setActiveComponent(component);
+  };
+
+  const toggleColor = () => {
+    setColorToggle(!colorToggle);
+  };
+
+  const colorBoxAnimation = {
+  animation: 'fade-in 2s ease-out', // Customize the animation duration and timing function
+  };
+  
+  const colorBlockStyleMooo = {
+    ...colorBoxAnimation,
+    display:
+    activeComponent === 'Mooo' || activeComponent === null ? 'inline-block' : 'none',
+    padding: '10px',
+    background: `linear-gradient(to right, ${colorToggle ? getRandomPastelColor() : randomPastelColorMooo} 0%, ${colorToggle ? getRandomPastelColor() : randomPastelColorMooo} 51%, ${colorToggle ? getRandomPastelColor() : randomPastelColorMooo} 100%)`,
+    textDecoration: 'none',
+    height: '600px',
+    width: '600px',
+    transform: 'translateY(-80%)',
+    marginLeft: '5%',
+    marginTop: '5%',
+  };
+  
+  const colorBlockStyleYogi = {
+    ...colorBoxAnimation,
+    display:
+    activeComponent === 'Yogi' || activeComponent === null ? 'inline-block' : 'none',
+    padding: '10px',
+    background: `linear-gradient(to right, ${colorToggle ? getRandomPastelColor() : randomPastelColorYogi} 0%, ${colorToggle ? getRandomPastelColor() : randomPastelColorYogi} 51%, ${colorToggle ? getRandomPastelColor() : randomPastelColorYogi} 100%)`,
+    textDecoration: 'none',
+    height: '600px',
+    width: '600px',
+    transform: 'translateY(-80%)',
+    marginLeft: '5%',
+    marginTop: '5%',
+  };
+
+  const colorBlockStyleStudio = {
+    ...colorBoxAnimation,
+    display: activeComponent === 'Studio' || activeComponent === null ? 'inline-block' : 'none',
+    padding: '10px',
+    background: `linear-gradient(to right, ${colorToggle ? getRandomPastelColor() : randomPastelColorStudio} 0%, ${colorToggle ? getRandomPastelColor() : randomPastelColorStudio} 51%, ${colorToggle ? getRandomPastelColor() : randomPastelColorStudio} 100%)`,
+    textDecoration: 'none',
+    height: '600px',
+    width: '600px',
+    transform: 'translateY(-80%)',
+    marginLeft: '5%',
+    marginTop: '5%',
+    };
+
+    if (window.matchMedia('(min-width: 1920)').matches) {
+      colorBlockStyleMooo.marginLeft = '50%';
+      colorBlockStyleMooo.marginRight = 'auto';
+
+      colorBlockStyleYogi.marginLeft = 'auto';
+      colorBlockStyleYogi.marginRight = 'auto';
+
+      colorBlockStyleStudio.marginLeft = 'auto';
+      colorBlockStyleStudio.marginRight = 'auto';
+    }
+
+    const styles = `
+  @keyframes fade-in {
+    from {
+      opacity: 20%;
+    }
+    to {
+      opacity: 10;
+    }
+  }
+`;
+
+// Apply the styles to the document head
+const styleSheet = document.styleSheets[0];
+styleSheet.insertRule(styles, styleSheet.cssRules.length);
+
+
+
+  return (
+    <body>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+        <header id = "app-header">
+        <img id="logo" src={process.env.PUBLIC_URL + '/cateye.PNG'} alt="Cat's Eye Vu Studio Logo" />
+          {/* <label id="dev">Cat's Eye Vu Studio</label> */}
+          <div className="header-content">
+          <Router>
+           <Link to="/Login" className='home-link'>
+            <div className="home-button" onClick={() => toggleComponent(null)}>
+              Home
+            </div> </Link>
+            <ScrollLink to="about"
+            spy={true}
+            smooth={true}
+            offset={-70} // Adjust the offset based on your fixed header's height
+            duration={500}
+            className='about'
+          >
+  <div className="home-button" onClick={() => toggleComponent(null)}>
+    About
+  </div>
+</ScrollLink>
+
+<ScrollLink
+          to="contact-footer"
+          spy={true}
+          smooth={true}
+          offset={-70} // Adjust the offset based on your fixed header's height
+          duration={500}
+          className='contact'
+        >
+          <div className="home-button" onClick={() => toggleComponent(null)}>
+            Contact
+          </div>
+        </ScrollLink>
+
+           </Router>
+            {/* <div>Contact</div> */}
+          </div>
+        </header>
+
+      <div className = "content">
+        <div className="intro">
+          <div>
+            <h1 id="portfolio">
+              Greetings, I am Cat Tuong Vu, and you can call me Kate. 
+              I'm delighted to have you on my page!
+            </h1>
+          </div>
+          <img
+            id="kate"
+            src={process.env.PUBLIC_URL + '/kate.png'}
+            alt="Kate's portrait"
+          />
+
+        </div>
+
+        <Element name="about" className="section-container about" id="about">
+  <h2 className="section-heading">About Me</h2>
+  <p className="section-content">
+    With my passion for UX/UI and front-end development, 
+    I thrive on transforming ideas into engaging and impactful user experiences across web apps and mobile apps. 
+    My approach to projects involves conducting studies and research, alongside coding. Additionally, 
+    I am also an IT/Cybersecurity specialist at the San Francisco Housing Authority, where I 
+    integrate my knowledge of user experience to tailor solutions while assisting individuals with computers. 
+    My journey into the Tech and IT industry has equipped me with the keen eyes for detail, a love for problem-solving, 
+    and a commitment to delivering high-quality results.
+   </p>
+        </Element>
+
+<div>
+      <div>
+      <p id = "generator">"Hmmm...I am indecisive about the boxes' color.
+      So, I made a little color generator for viewers to play with"</p>
+      <button className="ToggleButton" onClick={toggleColor}>
+          Generate Color
+      </button>
+      </div>
+
+      <div className="ColorBlock" >
+      <Router>
+        <Link
+            to="/Mooo"
+            className="color-block"
+            style={colorBlockStyleMooo}
+            onClick={() => toggleComponent('Mooo')}
+          >
+          <img
+            src={process.env.PUBLIC_URL + '/Mooo.png'} 
+            alt="Go to Mooo"
+            style={{ width: '100%', height: '100%', borderRadius: '5px' }} 
+          />
+          <p className="color-block-description">Mooo - Movie Theater Snack Ordering App - Webflow - User Interface</p>
+        </Link>
+       
+        <Link
+            to="/Yogi"
+            className="color-block"
+            style={colorBlockStyleYogi}
+            onClick={() => toggleComponent('Yogi')}
+          >
+          <img
+            src={process.env.PUBLIC_URL + '/Yogi.png'} 
+            alt="Go to Yogi"
+            style={{ width: '100%', height: '100%', borderRadius: '5px' }}
+          />
+          <p className="color-block-description">Yogi - User Interface - Branding - Frontend Development</p>
+        </Link>
+
+        <Link
+            to="/Studio"
+            className="color-block"
+            style={colorBlockStyleStudio}
+            onClick={() => toggleComponent('Studio')}
+          >
+          <img
+            src={process.env.PUBLIC_URL + '/eye.PNG'} 
+            alt="Go to Studio"
+            style={{ width: '85%', height: '55%', borderRadius: '5px', paddingLeft: '40px', marginTop: '100px' }} 
+          />
+          <p className="color-block-description">Cat's Eye Vu Studio - Logo Design - Branding</p>
+        </Link>
+
+        <Routes>
+          {activeComponent === 'Mooo' && (
+              <Route path="/Mooo" element={<Mooo />} />
+            )}
+          {activeComponent === 'Yogi' && (
+              <Route path="/Yogi" element={<Yogi />} />
+            )}
+          {activeComponent === 'Studio' && (
+              <Route path="/Studio" element={<Studio />} />
+            )}  
+        </Routes>
+      </Router>
+      
+    </div> 
+  
+      <Element name="contact" id="contact">
+          <ContactFooter />
+        </Element>
+      </div>  
+      </div>
+    </body>
+    
+  );
+}
+
+export default App;
---
+++ src/Studio.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Studio.css';
+
+function Studio() {
+  return (
+    <div className = "studio-content">
+      <p id = "p1">
+      During a conversation with my coworker, Ed, about my personal webpage, 
+      I mentioned that the initial name for my site would be "The Vu Studio." 
+    My concept revolves around having a name that is specific enough to reflect 
+    my personality, so it should have my last name - Vu. Additionally, a name that 
+    is broad enough to encompass both my design and development works. We then engaged 
+    in a bit of wordplay with my Viet name, Cat Tuong Vu. Given that "Vu" sounds similar 
+    to "view" and considering that my friends often associate me with cats, known as my 
+    spirited animal, we drew inspiration from the English term "Bird’s eye view." 
+    As a result, Cat’s Eye Vu Studio.
+      </p>
+      <div className='logo-details'>
+      <img id="ed" src={process.env.PUBLIC_URL + '/Ed.jpg'} alt="Ed idea" />
+      <p id = "p2">
+      Ed went above and beyond, providing me with some design ideas for my logo, 
+      which later inspired my own creation.
+      </p>
+    
+      <img id="eyesketch" src={process.env.PUBLIC_URL + '/eyesketch.jpg'} alt="eye sketch" />
+      <p id = "p3">
+      Looking at the hand sketch that Ed gave me, I really loved how the eyes in the “studio” looked, so I decided to 
+      simplify my logo. I threw in my initials, CV, inside the eye, giving 
+      it that cool cat's pupil vibe. Ed was like, “Wow, that's pretty impressive, 
+      especially since you whipped it up in just half an hour.” As a person 
+      who doesn't have any background in logo design, Ed's kind words really 
+      fueled my passion to keep doing my thing.
+      </p>
+      
+      <img id="letters" src={process.env.PUBLIC_URL + '/letters.jpg'} alt="letters sketch" />
+      <img id="eye" src={process.env.PUBLIC_URL + '/eye.PNG'} alt="eye logo" />  
+      <p id = "p5">
+      The font I selected for my logo was Palatino-Italic, chosen for its 'o' 
+      resembling a cat's eye. I slightly rotated the 'o' and incorporated my initials. 
+      I drew a small loop for the 'V' and colored the upper part of the pupil to give 
+      the cat's eye a starry appearance. Initially, I considered leaving the eye's 
+      ending open, but I believed closing it with black color would enhance the 
+      highlights of my logo
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default Studio;
